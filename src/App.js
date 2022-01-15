8d8b9d40741ad3ad42565435968857e1b3d2b88e
Luke <kineticstorytelling@gmail.com>
2022-01-15 15:58:11 -0500

    todolist add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+
+export default function App() {
+  const [todos, setTodos] = React.useState([
+    { id: 1, text: "Do Homework", done: false },
+    { id: 2, text: "Buy groceries", done: false },
+    { id: 3, text: "Sweep floors", done: false }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todolist'>
+      <h1>Todo List</h1>
+      <TodoList todos={todos} />
+      <AddTodo setTodos={setTodos} />
     </div>
   );
 }
 
-export default App;
+function TodoList({ todos }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.text}</li>
+      ))}
+    </ul>
+  );
+}
+
+function AddTodo({setTodos}) {
+  const inputRef = React.useRef();
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const text = event.target.elements.addTodo.value;
+    const todo ={
+      id: 4,
+      text,
+      done: false
+    };
+    setTodos(prevTodos => {
+      return prevTodos.concat(todo)
+    })
+    inputRef.current.value = "";
+  }
+
+  return (
+    <form onSubmit={handleAddTodo}>
+      <input name="addTodo" placeholder="Add todo" ref={inputRef} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
